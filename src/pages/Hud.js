303b581296e2bcf97c4b83bf180789df12ba2496
Melvin Melendrez <melvin_sss@hotmail.com>
2024-01-06 22:24:16 -0700

    update

--- src/pages/Hud.js
+++ src/pages/Hud.js
@@ -3,7 +3,6 @@ import React from 'react'
 import { Hud2 } from '../components/Hud2';
 import Hud1 from '../components/Hud1';
 import SocialButtons from '../components/SocialButtons'
-import Spinner from '../components/Spinner'
 
 
 const Hud = () => {
