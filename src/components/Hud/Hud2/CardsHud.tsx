17553183aa4ac1a1c915b5b3a08e1a92ff57beea
MelvinMelendrez <melvin_sss@hotmail.com>
2024-08-08 11:50:31 -0700

    projects update

--- src/components/Hud/Hud2/CardsHud.tsx
+++ src/components/Hud/Hud2/CardsHud.tsx
@@ -23,21 +23,21 @@ const CardsHud: React.FC = () => {
       visit: 'https://next-animated-portfolio.vercel.app/'
     },
     {
-      title: "Melvin's portfolio",
-      subtitle: 'React',
-      image:
-        'https://cdn.sanity.io/images/ohx8wpby/production/beeb006981c79f72ad02ca581f1c5fb32f5dde8b-696x822.png',
-      color: '#F44336',
-      gitHub: 'https://github.com/MelvinMelendrez/melvin-s-portfolio',
-      visit: 'https://melvinapp.com'
-    },
-    {
-      title: "food blog",
+      title: "Food blog",
       subtitle: 'Vue',
       image: 'https://cdn.sanity.io/images/ohx8wpby/production/7b7b5bf1fed4daee46fc41b258792bbfe9279617-1440x1714.jpg',
       color: '#F44336',
       gitHub: 'https://github.com/MelvinMelendrez/Vue-food-blog',
       visit: 'https://vue-food-blog.vercel.app/'
+    },
+    {
+      title: 'Go Travel',
+      subtitle: 'React Native',
+      image:
+        'https://cdn.sanity.io/images/ohx8wpby/production/782022d291278a6c2de1ef15d35df30aaef7a5dc-370x1027.jpg',
+      color: '#4CAF50',
+      gitHub: 'https://github.com/MelvinMelendrez/Go-Travel.git',
+      visit: 'https://cdn.sanity.io/images/ohx8wpby/production/9b2898a654a087fa109ff6ccbf6294de27c53636-702x1024.jpg'
     },
     {
       title: 'Healty blog',
@@ -48,6 +48,15 @@ const CardsHud: React.FC = () => {
       gitHub: 'https://github.com/MelvinMelendrez/Blog.git',
       visit: 'https://blog-six-ivory.vercel.app/'
     },
+    {
+      title: "Melvin's portfolio",
+      subtitle: 'React',
+      image:
+        'https://cdn.sanity.io/images/ohx8wpby/production/beeb006981c79f72ad02ca581f1c5fb32f5dde8b-696x822.png',
+      color: '#F44336',
+      gitHub: 'https://github.com/MelvinMelendrez/melvin-s-portfolio',
+      visit: 'https://melvinapp.com'
+    },
     // {
     //   title: 'Melvin portfolio',
     //   subtitle: 'React',
@@ -57,15 +66,6 @@ const CardsHud: React.FC = () => {
     //   gitHub: 'https://github.com/MelvinMelendrez/portfolio',
     //   visit: 'https://portfolio-git-main-krispis0.vercel.app/'
     // },
-    {
-      title: 'Go Travel',
-      subtitle: 'React Native',
-      image:
-        'https://cdn.sanity.io/images/ohx8wpby/production/782022d291278a6c2de1ef15d35df30aaef7a5dc-370x1027.jpg',
-      color: '#4CAF50',
-      gitHub: 'https://github.com/MelvinMelendrez/Go-Travel.git',
-      visit: 'https://cdn.sanity.io/images/ohx8wpby/production/9b2898a654a087fa109ff6ccbf6294de27c53636-702x1024.jpg'
-    }
   ];
 
   return (
